8a749b4e65c81e51cfa3a76ac432158c626e5a16
Aniket <aniketvam02@gmail.com>
2026-09-03 19:30:53 +0530

    test

--- question_solving/01_single_code.py
+++ question_solving/01_single_code.py
@@ -8,4 +8,4 @@ num=int(input("Enter the number:"))
 if num <=1:
     print("Enter the number greater than 1")
 else:
-    print(f"The sum of {num} natural number is {sum(num)}.")
+    print(f"The sum of {num} natural number is {sum(num)}.")  
